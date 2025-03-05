ba089044d995162dab3b5166e8f323ca4ef95b30
Pavel_Karpovich <pavel.karpovich@succraft.com>
2025-03-05 17:36:53 +0300

    Done

--- client/frontend.js
+++ client/frontend.js
@@ -1,37 +1,82 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { createApp, defineComponent } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+const Loader = defineComponent({
+    template: `
+        <div style="display: flex; justify-content: center; align-items: center">
+            <div class="spinner-border" role="status">
+
+            </div>  
+        </div>
+    `
+});
+
 
 createApp({
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
             },
             contacts: [
-                {id: 1, name: 'Vladilen', value: '+7-921-100-20-30', marked: false}
+                
             ]
         };
     },
     methods: {
-        createContact() {
+        async createContact() {
             const {...contact} = this.form
-            console.log(contact)
-
-            this.contacts.push({...contact, id: Date.now(), marked: false})
+            
+            const newContact = await request('api/contacts', 'POST', contact)
+            this.contacts.push(newContact)
 
             this.form.name = this.form.value = ''
         },
-        markContact(id) {
+        async markContact(id) {
             const contact = this.contacts.find(c => c.id === id)
-            contact.marked = true
+            const updated = await request(`api/contacts/${id}`, 'PUT', {
+                ...contact, 
+                marked: true
+            })
+            contact.marked = updated.marked
         },
-        removeContact(id) {
+        async removeContact(id) {
+            await request(`api/contacts/${id}`, 'DELETE')
             this.contacts = this.contacts.filter(c => c.id !== id)     
+           
         }
     },
     computed: {
         canCreate() {
             return this.form.name && this.form.value
         }
-    }
+    },
+    async mounted() {
+        this.loading = true
+        this.contacts = await request('/api/contacts')
+        this.loading = false
+    },
+    components: { Loader }
 }).mount('#app');
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch (e) {
+        console.warn(e.message)
+    }
+}
